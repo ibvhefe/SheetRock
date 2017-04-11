c2bff3b93599958551a3d4d5375d36d4a3b39d7f
Sebi <sebastian.mechelke@hotmail.de>
2017-04-11 23:17:59 +0200

    Added cmake and makefiles

---
+++ src/CMakeFiles/StartSheetRock.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StartSheetRock.dir/StartSheetRock.cpp.o"
+  "StartSheetRock.pdb"
+  "StartSheetRock"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StartSheetRock.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
