55e4229eb322a6add20e40068536ee9f1208bf09
Sebi <sebastian.mechelke@hotmail.de>
2017-04-17 01:15:05 +0200

    Added another sample file. Show image when starting SheetRock

--- src/StartSheetRock.cpp
+++ src/StartSheetRock.cpp
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
+#include <string>
+#include <sstream>
 using namespace cv;
 int main(int argc, char** argv )
 {
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
+    std::stringstream ss;
+    ss << "../samples/sample";
+    ss << argv[1];
+    ss << ".jpg";
+    std::string filename;
+    ss >> filename;
     Mat image;
-    image = imread( argv[1], 1 );
+    image = imread( filename, 1 );
     if ( !image.data )
     {
         printf("No image data \n");
