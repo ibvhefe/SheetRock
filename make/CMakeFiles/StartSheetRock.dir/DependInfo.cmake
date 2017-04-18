1388f603e4dbe062d84fa66e9268d2e648f69bea
Sebi <sebastian.mechelke@hotmail.de>
2017-04-18 22:37:18 +0200

    moved make generated files to own folder

--- make/CMakeFiles/StartSheetRock.dir/DependInfo.cmake
+++ make/CMakeFiles/StartSheetRock.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/sebi/Desktop/SheetRock/src/StartSheetRock.cpp" "/home/sebi/Desktop/SheetRock/src/CMakeFiles/StartSheetRock.dir/StartSheetRock.cpp.o"
+  "/home/sebi/Desktop/SheetRock/src/StartSheetRock.cpp" "/home/sebi/Desktop/SheetRock/make/CMakeFiles/StartSheetRock.dir/StartSheetRock.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
